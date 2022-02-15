31913d8dadbe88d25b48a6e2cf4a4f76bf0617aa
Eric Romano <ecr1980@gmail.com>
2022-02-15 02:27:24 -0500

    Removed the require pry comment. Cleaned up code formatting.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,10 @@
-#require 'pry-byebug'
-
 test_array = [4,3,78,2,0,2, 19, 67, 3, 5, 9, 43, 193, 83, 74]
 puts test_array.to_s
 
 def bubble_sort(input_array)
-
-  
-
   input_array.each do
     input_array.each_index do |index|
-      if (index < input_array.length-1) 
+      if (index < input_array.length-1)         
         if input_array[index] < input_array[index + 1]
           temp = input_array[index]
           input_array[index] = input_array[index + 1]
@@ -18,8 +13,7 @@ def bubble_sort(input_array)
       end
     end
   end
-    
-  end
+end
 
 
 puts bubble_sort(test_array).to_s
