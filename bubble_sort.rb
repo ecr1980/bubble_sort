2910b9bdf7bcb66338bb2c07a90bcbd72a00b245
Eric Romano <ecr1980@gmail.com>
2022-02-15 02:18:25 -0500

    Added basic bubble sort method. This does not stop running if the sorting completes early.

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,26 @@
-test_array = [4,3,78,2,0,2]
+#require 'pry-byebug'
+
+test_array = [4,3,78,2,0,2, 19, 67, 3, 5, 9, 43, 193, 83, 74]
+puts test_array.to_s
 
 def bubble_sort(input_array)
-  output_array = input_array
-  puts output_array.to_s
-end
 
-bubble_sort(test_array)
+  
+
+  input_array.each do
+    input_array.each_index do |index|
+      if (index < input_array.length-1) 
+        if input_array[index] < input_array[index + 1]
+          temp = input_array[index]
+          input_array[index] = input_array[index + 1]
+          input_array[index + 1] = temp
+        end
+      end
+    end
+  end
+    
+  end
+
+
+puts bubble_sort(test_array).to_s
+
